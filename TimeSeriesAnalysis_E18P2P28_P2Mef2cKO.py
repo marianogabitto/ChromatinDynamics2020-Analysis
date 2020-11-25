27836f36c3dba69e0d354d2c97e2af1456146aa7
gs512 <g.saldi@gmail.com>
2020-11-25 23:52:58 +0900

    some pep8 fix

--- TimeSeriesAnalysis_E18P2P28_P2Mef2cKO.py
+++ TimeSeriesAnalysis_E18P2P28_P2Mef2cKO.py
@@ -1,6 +1,7 @@
 # ###################################################################################################
 # ###################################################################################################
-# Script to read Gene Regulatory Network information and Generate Panels in the figures.
+# Script to read Gene Regulatory Network information and Generate Panels in the
+# figures.
 # Joint Work: Giuseppe Saldi and Mariano Gabitto
 # ###################################################################################################
 # ###################################################################################################
@@ -11,19 +12,19 @@ from functions import *
 # ############################################################################################################
 # READING inputs
 
-nets = {'P28_PV' :'Data/P28PV.tsv',
-        'P28_SST':'Data/P28SST.tsv',
-        'E18_PV' :'Data/E18PV.tsv',
-        'E18_SST':'Data/E18SST.tsv',
-        'P2_PV'  :'Data/16NOVP2PVnet.tsv',
-        'P2_SST' :'Data/16NOVP2SSTnet.tsv'}
+nets = {'P28_PV':  'Data/P28PV.tsv',
+        'P28_SST': 'Data/P28SST.tsv',
+        'E18_PV':  'Data/E18PV.tsv',
+        'E18_SST': 'Data/E18SST.tsv',
+        'P2_PV':   'Data/16NOVP2PVnet.tsv',
+        'P2_SST':  'Data/16NOVP2SSTnet.tsv'}
 # RNA
-paths = {'P28_exp':'Data/P28_dwk.h5ad',
-         'E18_exp':'Data/E18_Lhx6.h5ad',
-         'P2_exp' :'Data/nov15_P2_noftt.h5ad',
-         'tf_list':'Data/NOV6_aug_mus_tf.txt'}
+paths = {'P28_exp': 'Data/P28_dwk.h5ad',
+         'E18_exp': 'Data/E18_Lhx6.h5ad',
+         'P2_exp':  'Data/nov15_P2_noftt.h5ad',
+         'tf_list': 'Data/NOV6_aug_mus_tf.txt'}
 
-grns = {x:read_and_filter_grn(nets[x],var=.0,net=x) for x in nets}
+grns = {x: read_and_filter_grn(nets[x], var=.0, net=x) for x in nets}
 adatas = load_expression(paths)
 fraction = .01
 
@@ -41,10 +42,11 @@ values = [len(set(grns['E18_SST'].target)), len(set(grns['E18_PV'].target)),
 
 pp = PdfPages("panelA_number_expressed_genes.pdf")
 plt.xticks(rotation=45)
-plt.bar(['E18_SST {}'.format(values[0]), 'E18_PV {}'.format(values[1]), 'P2-E18_SST {}'.format(values[2]),
-         'P2-E18_PV {}'.format(values[3]), 'P2_SST {}'.format(values[4]), 'P2_PV {}'.format(values[5]),
-         'P2-P28_SST {}'.format(values[6]), 'P2-P28_PV {}'.format(values[7]), 'P28_SST {}'.format(values[8]),
-         'P28_PV {}'.format(values[9])],
+plt.bar(['E18_SST {}'.format(values[0]), 'E18_PV {}'.format(values[1]),
+         'P2-E18_SST {}'.format(values[2]),'P2-E18_PV {}'.format(values[3]),
+         'P2_SST {}'.format(values[4]), 'P2_PV {}'.format(values[5]),
+         'P2-P28_SST {}'.format(values[6]), 'P2-P28_PV {}'.format(values[7]),
+         'P28_SST {}'.format(values[8]),'P28_PV {}'.format(values[9])],
         values)
 plt.ylabel("Number of Expressed Genes")
 plt.tight_layout()
@@ -66,9 +68,10 @@ for gr in grns:
 
 pp = PdfPages("panelB_total_number_edges.pdf")
 markers = ['o', 'o', '^', '^', 's', 's']
-colors  = ['#5A738B','#B8767E','#5A738B','#B8767E','#5A738B','#B8767E']
+colors = ['#5A738B', '#B8767E', '#5A738B', '#B8767E', '#5A738B', '#B8767E']
 for i_, gr in enumerate(tot.keys()):
-    plt.scatter(v_range, tot[gr], alpha=0.6, label=gr, marker=markers[i_], c=colors[i_])
+    plt.scatter(v_range, tot[gr], alpha=0.6, label=gr, marker=markers[i_],
+                c=colors[i_])
 plt.legend(loc='upper right', shadow=False)
 plt.xlabel('variance explained')
 plt.ylabel('# edges')
@@ -81,15 +84,18 @@ pp.close()
 # ############################################################################################################
 # PANEL D: SHARD EDGES PV AND SST AT EACH TIME POINT
 variance_range = np.arange(0.01, 0.41, 0.01)
-e18 = shared_edges_variance({'E18_SST': grns['E18_SST'], 'E18_PV': grns['E18_PV']}, variance_range)
-p2  = shared_edges_variance({'P2_SST' : grns['P2_SST'],   'P2_PV': grns['P2_PV' ]}, variance_range)
-p28 = shared_edges_variance({'P28_SST': grns['P28_SST'], 'P28_PV': grns['P28_PV']}, variance_range)
+e18 = shared_edges_variance({'E18_SST': grns['E18_SST'],
+                            'E18_PV': grns['E18_PV']}, variance_range)
+p2 = shared_edges_variance({'P2_SST':  grns['P2_SST'],
+                            'P2_PV': grns['P2_PV']}, variance_range)
+p28 = shared_edges_variance({'P28_SST': grns['P28_SST'],
+                             'P28_PV': grns['P28_PV']}, variance_range)
 
 pp = PdfPages("panelC_shared_edges_pv_sst.pdf")
-plt.scatter(variance_range, p28, alpha=0.6, marker='o',c='#999' )
-plt.scatter(variance_range, p2,  alpha=0.6, marker='s',c='#999' )
-plt.scatter(variance_range, e18, alpha=0.6, marker='^',c='#999' )
-plt.legend(loc='upper right', shadow=False,labels=['P28','P2','E18'])
+plt.scatter(variance_range, p28, alpha=0.6, marker='o', c='#999')
+plt.scatter(variance_range, p2,  alpha=0.6, marker='s', c='#999')
+plt.scatter(variance_range, e18, alpha=0.6, marker='^', c='#999')
+plt.legend(loc='upper right', shadow=False, labels=['P28', 'P2', 'E18'])
 plt.xlabel('variance explained')
 plt.ylabel('# shared edges')
 plt.tight_layout()
@@ -101,18 +107,25 @@ pp.close()
 # ############################################################################################################
 # PANEL E: FRACTION OF UNIQUE EDGES PV AND SST AT EACH TIME POINT
 pp = PdfPages("panelD_fraction_unique_edges_pv_sst.pdf")
-plt.scatter(variance_range, (np.array(tot['E18_SST']) - np.array(e18)) / np.array(tot['E18_SST']),
-            alpha=0.6, marker='^', color='#B8767E', label="E18 SST")
-plt.scatter(variance_range, (np.array(tot['E18_PV']) - np.array(e18)) / np.array(tot['E18_PV']),
-            alpha=0.6, marker='^', color='#5A738B', label="E18 PV")
-plt.scatter(variance_range, (np.array(tot['P2_SST']) - np.array(p2)) / np.array(tot['P2_SST']),
-            alpha=0.6, marker='s', color='#B8767E', label="P2 SST")
-plt.scatter(variance_range, (np.array(tot['P2_PV']) - np.array(p2)) / np.array(tot['P2_PV']),
-            alpha=0.6, marker='s', color='#5A738B', label="P2 PV")
-plt.scatter(variance_range, (np.array(tot['P28_SST']) - np.array(p2)) / np.array(tot['P28_SST']),
-            alpha=0.6, marker='o', color='#B8767E', label="P28 SST")
-plt.scatter(variance_range, (np.array(tot['P28_PV']) - np.array(p2)) / np.array(tot['P28_PV']),
-            alpha=0.6, marker='o', color='#5A738B', label="P28 PV")
+plt.scatter(variance_range, (np.array(tot['E18_SST']) - np.array(e18)) /
+            np.array(tot['E18_SST']),alpha=0.6, marker='^', color='#B8767E',
+            label="E18 SST")
+plt.scatter(variance_range, (np.array(tot['E18_PV']) - np.array(e18)) /
+            np.array(tot['E18_PV']), alpha=0.6, marker='^', color='#5A738B',
+            label="E18 PV")
+plt.scatter(variance_range, (np.array(tot['P2_SST']) - np.array(p2)) /
+            np.array(tot['P2_SST']), alpha=0.6, marker='s', color='#B8767E',
+            label="P2 SST")
+plt.scatter(variance_range, (np.array(tot['P2_PV']) - np.array(p2)) /
+            np.array(tot['P2_PV']), alpha=0.6, marker='s', color='#5A738B',
+            label="P2 PV")
+plt.scatter(variance_range, (np.array(tot['P28_SST']) - np.array(p2)) /
+            np.array(tot['P28_SST']), alpha=0.6, marker='o', color='#B8767E',
+            label="P28 SST")
+plt.scatter(variance_range, (np.array(tot['P28_PV']) - np.array(p2)) /
+            np.array(tot['P28_PV']), alpha=0.6, marker='o', color='#5A738B',
+            label="P28 PV")
+
 plt.legend(loc='best', shadow=False)
 plt.xlabel('variance explained')
 plt.ylabel('Fraction of unique edges')
@@ -128,40 +141,57 @@ tbv = {}
 variance_range = np.arange(0.01, 0.41, 0.01)
 
 for net in grns:
-    tbv[net] = [targets_by_var(grns[net], var = _v) for _v in variance_range]
+    tbv[net] = [targets_by_var(grns[net], var=_v) for _v in variance_range]
 
-pv_shared_tbv  = [len(shared_targets_by_var({k: v for k,v in grns.items() if 'PV' in k}, _v) )for _v in variance_range]
-sst_shared_tbv = [len(shared_targets_by_var({k: v for k,v in grns.items() if 'SST' in k}, _v)) for _v in variance_range]
-all_shared_tbv = [len(shared_targets_by_var(grns, _v)) for _v in variance_range]
+pv_shared_tbv = [len(shared_targets_by_var({k: v for k, v in grns.items() if
+                 'PV' in k}, _v))for _v in variance_range]
+sst_shared_tbv = [len(shared_targets_by_var({k: v for k, v in grns.items() if
+                  'SST' in k}, _v)) for _v in variance_range]
+all_shared_tbv = [len(shared_targets_by_var(grns, _v)) for _v in
+                  variance_range]
 
 ese = {}
-ese['PV'] = pd.DataFrame(zip(tbv['P28_PV'],tbv['P2_PV'],tbv['E18_PV'],
+ese['PV'] = pd.DataFrame(zip(tbv['P28_PV'], tbv['P2_PV'], tbv['E18_PV'],
                          pv_shared_tbv, all_shared_tbv),
-                         columns=['P28','P2','E18','pv_shared','all_shared'],
-                         index=variance_range)
-ese['SST'] = pd.DataFrame(zip(tbv['P28_SST'],tbv['P2_SST'],tbv['E18_SST'],
+                         columns=['P28', 'P2', 'E18', 'pv_shared',
+                         'all_shared'], index=variance_range)
+ese['SST'] = pd.DataFrame(zip(tbv['P28_SST'], tbv['P2_SST'], tbv['E18_SST'],
                           pv_shared_tbv, all_shared_tbv),
-                          columns=['P28','P2','E18','sst_shared','all_shared'],
-                          index=variance_range)
+                          columns=['P28', 'P2', 'E18', 'sst_shared',
+                          'all_shared'],index=variance_range)
 
 fig, axs = plt.subplots(ncols=2)
 
 ax, ax1 = axs.flatten()
-pla = ax.scatter(ese['PV'].index.values,ese['PV']['P28'] ,alpha=0.6, marker='o', c='#5A738B')
-plb = ax.scatter(ese['PV'].index.values,ese['PV']['P2']  ,alpha=0.6, marker='s', c='#5A738B')
-plc = ax.scatter(ese['PV'].index.values,ese['PV']['E18'] ,alpha=0.6, marker='^', c='#5A738B')
-plg = ax.scatter(ese['PV'].index.values,ese['PV']['pv_shared'] ,alpha=0.6, marker='*', c='#5a8b77')
-plh = ax.scatter(ese['PV'].index.values,ese['PV']['all_shared'] ,alpha=0.6, marker='*', c='#658b5a')
-ax.legend((pla,plb,plc,plg,plh),('P28','P2','E18','PV shared','all shared'),loc='upper right', shadow=False)
+pla = ax.scatter(ese['PV'].index.values, ese['PV']['P28'], alpha=0.6,
+                 marker='o', c='#5A738B')
+plb = ax.scatter(ese['PV'].index.values, ese['PV']['P2'], alpha=0.6,
+                 marker='s', c='#5A738B')
+plc = ax.scatter(ese['PV'].index.values, ese['PV']['E18'], alpha=0.6,
+                 marker='^', c='#5A738B')
+plg = ax.scatter(ese['PV'].index.values, ese['PV']['pv_shared'], alpha=0.6,
+                 marker='*', c='#5a8b77')
+plh = ax.scatter(ese['PV'].index.values, ese['PV']['all_shared'], alpha=0.6,
+                 marker='*', c='#658b5a')
+ax.legend((pla, plb, plc, plg, plh), ('P28', 'P2', 'E18', 'PV shared',
+                                      'all shared'),loc='upper right',
+          shadow=False)
 ax.set_title('PV')
 
-pld = ax1.scatter(ese['SST'].index.values,ese['SST']['P28'],alpha=0.6, marker='o',c='#B8767E')
-ple = ax1.scatter(ese['SST'].index.values,ese['SST']['P2'] ,alpha=0.6, marker='s',c='#B8767E')
-plf = ax1.scatter(ese['SST'].index.values,ese['SST']['E18'],alpha=0.6, marker='^',c='#B8767E')
-pli = ax1.scatter(ese['SST'].index.values,ese['SST']['sst_shared'] ,alpha=0.6, marker='*', c='#5a8b77')
-plj = ax1.scatter(ese['SST'].index.values,ese['SST']['all_shared'] ,alpha=0.6, marker='*', c='#658b5a')
+pld = ax1.scatter(ese['SST'].index.values, ese['SST']['P28'], alpha=0.6,
+                  marker='o', c='#B8767E')
+ple = ax1.scatter(ese['SST'].index.values, ese['SST']['P2'], alpha=0.6,
+                  marker='s', c='#B8767E')
+plf = ax1.scatter(ese['SST'].index.values, ese['SST']['E18'], alpha=0.6,
+                  marker='^', c='#B8767E')
+pli = ax1.scatter(ese['SST'].index.values, ese['SST']['sst_shared'], alpha=0.6,
+                  marker='*', c='#5a8b77')
+plj = ax1.scatter(ese['SST'].index.values, ese['SST']['all_shared'], alpha=0.6,
+                  marker='*', c='#658b5a')
 
-ax1.legend((pld,ple,plf,pli,plj),('P28','P2','E18','SST shared','all shared'),loc='upper right', shadow=False)
+ax1.legend((pld, ple, plf, pli, plj), ('P28', 'P2', 'E18', 'SST shared',
+                                       'all shared'), loc='upper right',
+           shadow=False)
 ax1.set_title('SST')
 plt.tight_layout()
 # plt.ylabel('# edges')
@@ -169,14 +199,12 @@ plt.tight_layout()
 # plt.show()
 plt.savefig('panelF_Targets_among_networks.pdf')
 
-
-
 # ############################################################################################################
 # ############################################################################################################
 # ############################################################################################################
 # PANEL G: UNIQUE CELL TYPE EDGES FOR NON SPECIFIC TFS
 
-dedf ={}
+dedf = {}
 seltf = {}
 
 var = .05
@@ -184,12 +212,15 @@ f_grns = {}
 for grn in grns:
     f_grns[grn] = grns[grn][grns[grn]['var.exp.median'] >= var]
 
-seltf['P28'] = set(set(f_grns['P28_PV'].regulator) | set(f_grns['P28_SST'].regulator))
-seltf['P2']  = set(set(f_grns['P2_PV' ].regulator) | set(f_grns['P2_SST' ].regulator))
-seltf['E18'] = set(set(f_grns['E18_PV'].regulator) | set(f_grns['E18_SST'].regulator))
+seltf['P28'] = set(set(f_grns['P28_PV'].regulator) |
+                   set(f_grns['P28_SST'].regulator))
+seltf['P2'] = set(set(f_grns['P2_PV'].regulator) |
+                  set(f_grns['P2_SST'].regulator))
+seltf['E18'] = set(set(f_grns['E18_PV'].regulator) |
+                   set(f_grns['E18_SST'].regulator))
 
 
-dedf  = get_de_info(adatas, seltf)
+dedf = get_de_info(adatas, seltf)
 
 seltf['P28'] = get_non_specific_tf(dedf['P28'])
 seltf['P2']  = get_non_specific_tf(dedf['P2'])
@@ -198,15 +229,18 @@ seltf['E18'] = get_non_specific_tf(dedf['E18'])
 rpl = compute_edge_fractions(f_grns)
 
 fig, axs = plt.subplots(ncols=3)
-ax, ax1,ax2 = axs.flatten()
+ax, ax1, ax2 = axs.flatten()
 t = 'E18'
-ax.hist(rpl[t][rpl[t].index.isin(seltf['E18'])].T*100, alpha=0.6,density=True,label=['E18 PV','E18 SST'],bins=10, color = ['#5A738B','#B8767E'])
+ax.hist(rpl[t][rpl[t].index.isin(seltf['E18'])].T*100, alpha=0.6, density=True,
+        label=['E18 PV', 'E18 SST'], bins=10, color=['#5A738B', '#B8767E'])
 ax.set_title('{}'.format(t))
 t = 'P2'
-ax1.hist(rpl[t][rpl[t].index.isin(seltf['P2'])].T*100, alpha=0.6,density=True,label=['P2 PV','P2 SST'],bins=10, color = ['#5A738B','#B8767E'])
+ax1.hist(rpl[t][rpl[t].index.isin(seltf['P2'])].T*100, alpha=0.6, density=True,
+         label=['P2 PV', 'P2 SST'], bins=10, color=['#5A738B', '#B8767E'])
 ax1.set_title('{}'.format(t))
 t = 'P28'
-ax2.hist(rpl[t][rpl[t].index.isin(seltf['P28'])].T*100, alpha=0.6,density=True,label=['P28 PV','P28 SST'], color = ['#5A738B','#B8767E'])
+ax2.hist(rpl[t][rpl[t].index.isin(seltf['P28'])].T*100, alpha=0.6, density=True,
+         label=['P28 PV', 'P28 SST'], color=['#5A738B', '#B8767E'])
 ax2.set_title('{}'.format(t))
 plt.tight_layout()
 plt.savefig('panelG_unique_celltype_edges_for_non_specific_TFS.pdf')
@@ -214,20 +248,22 @@ plt.savefig('panelG_unique_celltype_edges_for_non_specific_TFS.pdf')
 # ############################################################################################################
 # ############################################################################################################
 # ############################################################################################################
-# networks files for rendering 
+# networks files for rendering
 var = .05
 f_grns = {}
+cols = ['target', 'regulator', 'var.exp.median']
 for grn in grns:
     f_grns[grn] = grns[grn][grns[grn]['var.exp.median'] >= var]
-    tmp = f_grns[grn][['target','regulator','var.exp.median']]
-    tmp[['target','regulator','var.exp.median']].to_csv('ipynb/{}.tsv'.format(grn),sep='\t',index=None)
+    tmp = f_grns[grn][cols]
+    file_n = 'ipynb/{}.tsv'.format(grn)
+    tmp[cols].to_csv(file_n, sep='\t', index=None)
 
 
-e18s = f_grns['E18_PV'][f_grns['E18_PV'].id.isin(f_grns['E18_SST'].id)][['target','regulator','var.exp.median']]
-e18s.to_csv('ipynb/{}.tsv'.format('E18_shared'),sep='\t',index=None)
+e18s = f_grns['E18_PV'][f_grns['E18_PV'].id.isin(f_grns['E18_SST'].id)][cols]
+e18s.to_csv('ipynb/{}.tsv'.format('E18_shared'), sep='\t', index=None)
 
-p2s = f_grns['P2_PV'][f_grns['P2_PV'].id.isin(f_grns['P2_SST'].id)][['target','regulator','var.exp.median']]
-p2s.to_csv('ipynb/{}.tsv'.format('P2_shared'),sep='\t',index=None)
+p2s = f_grns['P2_PV'][f_grns['P2_PV'].id.isin(f_grns['P2_SST'].id)][cols]
+p2s.to_csv('ipynb/{}.tsv'.format('P2_shared'), sep='\t', index=None)
 
-p28s = f_grns['P28_PV'][f_grns['P28_PV'].id.isin(f_grns['P28_SST'].id)][['target','regulator','var.exp.median']]
-p28s.to_csv('ipynb/{}.tsv'.format('P28_shared'),sep='\t',index=None)
+p28s = f_grns['P28_PV'][f_grns['P28_PV'].id.isin(f_grns['P28_SST'].id)][cols]
+p28s.to_csv('ipynb/{}.tsv'.format('P28_shared'), sep='\t', index=None)
--- functions.py
+++ functions.py
@@ -10,10 +10,10 @@ from scipy.sparse import isspmatrix
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-
 def read_and_filter_grn(grn_path, var=0, ci=0, net=None):
     grn = pd.read_table(grn_path, header=0)
-    grn['id'] = grn[['regulator', 'target']].apply(lambda x: '{}_{}'.format(x[0], x[1]), axis=1)
+    grn['id'] = grn[['regulator', 'target']].apply(lambda x: '{}_{}'.format(
+        x[0], x[1]), axis=1)
     if var > 0:
         grn = grn[grn['var.exp.median'] >= var]
     if ci > 0:
@@ -49,28 +49,31 @@ def fraction_edges_variance(grns, var):
     pass
 
 
-def extract_tf(data, TF, obs_key = None, obs_value = None,my_layer='Ms'):
+def extract_tf(data, TF, obs_key=None, obs_value=None, my_layer='Ms'):
     sub = -1
     if(obs_key is not None and obs_key in data.obs.keys()):
         data = data[data.obs[obs_key].isin([obs_value])]
         if(isspmatrix(data.layers[my_layer])):
             data.layers[my_layer] = data.layers[my_layer].todense()
-        sub = pd.DataFrame(data.layers[my_layer][:,data.var.index.isin(TF[0])])
+        sub = pd.DataFrame(data.layers[my_layer][:,
+                           data.var.index.isin(TF[0])])
         sub.columns = data.var.index[data.var.index.isin(TF[0])]
-        sub.index=data.obs.index
+        sub.index = data.obs.index
     return(sub)
 
 
 def load_expression(paths):
-    adatas = {k.split('_')[0]:sc.read_h5ad(paths[k]) for k in paths if '_exp' in k}
+    adatas = {k.split('_')[0]: sc.read_h5ad(paths[k]) for k in paths if
+              '_exp' in k}
     adatas['P2'].obs['cardinal_class'] = adatas['P2'].obs['CellType']
     for tp in adatas:
-        adatas[tp] = adatas[tp][:,adatas[tp].var.pct_dropout_by_counts != 100]
-        adatas[tp] = adatas[tp][adatas[tp].obs.cardinal_class.isin(['PV', 'SST'])]
-        sc.tl.rank_genes_groups(adatas[tp], groupby='cardinal_class',rankby_abs=True,
-                                pts = True,use_raw=False, layers='normed',
-                                method='wilcoxon',n_genes = adatas[tp].shape[1], 
-                                key_added = 'DE')
+        adatas[tp] = adatas[tp][:, adatas[tp].var.pct_dropout_by_counts != 100]
+        adatas[tp] = adatas[tp][adatas[tp].obs.cardinal_class.isin(['PV',
+                                                                   'SST'])]
+        sc.tl.rank_genes_groups(adatas[tp], groupby='cardinal_class',
+                                rankby_abs=True, pts=True, use_raw=False,
+                                layers='normed', method='wilcoxon',
+                                n_genes=adatas[tp].shape[1],key_added='DE')
 
     return adatas
 
@@ -78,16 +81,18 @@ def load_expression(paths):
 def targets_by_var(grn, var=.05, tfs=None):
     tmpg = grn
     if tfs is not None:
-        tmpg = grn[grn.regulator.isin(tfs.index.values)] 
-    tmpg = tmpg[tmpg['var.exp.median']>=var]
+        tmpg = grn[grn.regulator.isin(tfs.index.values)]
+    tmpg = tmpg[tmpg['var.exp.median'] >= var]
     return(len(set(tmpg.target)))
+
 
 def shared_targets(grns):
     edges = [grns[x].target for x in grns]
     ret = set(edges[0])
-    for x in range(len(edges)-1,0,-1):
+    for x in range(len(edges)-1, 0, -1):
         ret = ret & set(edges[x]) & set(edges[x-1])
     return(ret)
+
 
 def shared_targets_by_var(grns, by_var=.05):
     sub = {}
@@ -104,61 +109,71 @@ def get_de_info(adatas, seltf):
         gene_ids = adatas[tp].var.index.values
         if(isspmatrix(adatas[tp].layers['normed'])):
             obs = adatas[tp].layers['normed'].todense()
-        else: obs = adatas[tp].layers['normed'].copy()
-        obs = pd.DataFrame(obs,columns=gene_ids,index=adatas[tp].obs['cardinal_class'])
+        else:
+            obs = adatas[tp].layers['normed'].copy()
+        obs = pd.DataFrame(obs, columns=gene_ids,
+                           index=adatas[tp].obs['cardinal_class'])
         obs_bool = obs.astype(bool)
-        fraction_obs = obs_bool.groupby(level=0).sum()/obs_bool.groupby(level=0).count()
+        fraction_obs = obs_bool.groupby(level=0).sum()
+        fraction_obs /= obs_bool.groupby(level=0).count()
         mean_obs = obs.groupby(level=0).mean()
         mean_obs.loc['SST'][mean_obs.loc['SST'] == 0] = 1
-        mean_obs = pd.DataFrame(np.log2(mean_obs.loc['PV']/mean_obs.loc['SST']))
-        mean_obs.columns=['l2']
-        dedf[tp] = dedf[tp].merge(fraction_obs.T, left_on='names', right_index=True)
+        df = np.log2(mean_obs.loc['PV']/mean_obs.loc['SST'])
+        mean_obs = pd.DataFrame(df)
+        mean_obs.columns = ['l2']
+        dedf[tp] = dedf[tp].merge(fraction_obs.T, left_on='names',
+                                  right_index=True)
         dedf[tp] = dedf[tp].merge(mean_obs, left_on='names', right_index=True)
         dedf[tp] = dedf[tp][dedf[tp].names.isin(seltf[tp])]
     return dedf
 
-def get_non_specific_tf(dedf, lfc=.25, pval=.05,fpv=.1,fsst=.1):
-    return dedf[((abs(dedf.l2)<=lfc) | 
-                ((abs(dedf.l2)>lfc) & 
-                 (dedf.pvals>pval))) &
-                 ((dedf.PV >=fpv) | 
-                 (dedf.SST>=fsst))].names.values.tolist()
+
+def get_non_specific_tf(dedf, lfc=.25, pval=.05, fpv=.1, fsst=.1):
+    return dedf[((abs(dedf.l2) <= lfc) |
+                ((abs(dedf.l2) > lfc) &
+                 (dedf.pvals > pval))) &
+                ((dedf.PV >= fpv) |
+                 (dedf.SST >= fsst))].names.values.tolist()
+
 
 def compute_edge_fractions(f_grns):
     out_reg_s = {}
     tp = set([x.split('_')[0] for x in f_grns])
     for t in tp:
         report = {}
-        network_a, network_b = [x for x in f_grns.keys() if t == x.split('_')[0]]
+        network_a, network_b = [x for x in f_grns.keys() if
+                                t == x.split('_')[0]]
         networks = [network_a, network_b]
         cell_type_a, cell_type_b = [x.split('_')[1] for x in networks]
-        f_net_a  = f_grns[network_a]
-        f_net_b  = f_grns[network_b]
+        f_net_a = f_grns[network_a]
+        f_net_b = f_grns[network_b]
         for regulator in set(set(f_net_a.regulator) | set(f_net_b.regulator)):
-            n=0
+            n = 0
             edge_id = []
             if regulator in f_net_a.regulator.values:
                 n = f_net_a.regulator.value_counts()[regulator]
-                edge_id = list(f_net_a[f_net_a.regulator==regulator].id.values)
-            report[regulator] = {cell_type_a:{'n':n,'edge_id':edge_id}}
-            n=0
+                edge_id = list(f_net_a[f_net_a.regulator == regulator].id.values)
+            report[regulator] = {cell_type_a: {'n': n, 'edge_id': edge_id}}
+            n = 0
             edge_id = []
             if regulator in f_net_b.regulator.values:
                 n = f_net_b.regulator.value_counts()[regulator]
-                edge_id = list(f_net_b[f_net_b.regulator==regulator].id.values)
+                edge_id = list(f_net_b[f_net_b.regulator == regulator].id.values)
 
-            report[regulator][cell_type_b] = {'n': n , 'edge_id':edge_id}
-            
-            s_edges = set(report[regulator][cell_type_b]['edge_id']) & set(report[regulator][cell_type_a]['edge_id'])
+            report[regulator][cell_type_b] = {'n': n, 'edge_id': edge_id}
+            s_edges = set(set(report[regulator][cell_type_b]['edge_id']) &
+                          set(report[regulator][cell_type_a]['edge_id']))
             report[regulator]['shared_id'] = len(s_edges)
             divider = report[regulator][cell_type_a]['n']
-            if divider == 0: divider = 1
+            if divider == 0:
+                divider = 1
             report[regulator][cell_type_a]['prop_s'] = report[regulator]['shared_id'] / divider
             report[regulator][cell_type_a]['prop_u'] = None
             if report[regulator][cell_type_a]['n'] > 0:
                 report[regulator][cell_type_a]['prop_u'] = abs(1 - report[regulator][cell_type_a]['prop_s'])
-            divider = report[regulator][cell_type_b]['n'] 
-            if divider == 0: divider = 1
+            divider = report[regulator][cell_type_b]['n']
+            if divider == 0:
+                divider = 1
             report[regulator][cell_type_b]['prop_s'] = report[regulator]['shared_id'] / divider
             report[regulator][cell_type_b]['prop_u'] = None
             if report[regulator][cell_type_b]['n'] > 0:
@@ -166,13 +181,13 @@ def compute_edge_fractions(f_grns):
         out_reg_s[t] = report
 
     rpl = {}
-    
+
     for t in tp:
         rpl[t] = pd.DataFrame(zip([out_reg_s[t][x]['PV']['prop_u'] for x in out_reg_s[t].keys()],
                                   [out_reg_s[t][x]['SST']['prop_u'] for x in out_reg_s[t]]))
-        rpl[t].columns = ['PV','SST']
+        rpl[t].columns = ['PV', 'SST']
         rpl[t].index = out_reg_s[t].keys()
-    
+
     return rpl
 
 
@@ -181,11 +196,13 @@ def read_betas(t, cardinal_class, boot_path, n_boot=10):
             boot_path.format(t=t, b=b),
             index_col=0,header=0) for b in range(1, n_boot+1)}
 
+
 def B_dot_A(B, A, n_boot):
     acc = B['boot_1']
     for i in range(2, n_boot+1):
         acc += B['boot_{}'.format(i)]
     return acc.dot(A.T) / n_boot
+
 
 def write_reconstructed_exp(cell_names, gene_names, paths, exp):
     scio.mmwrite(paths['mtx'], sp.csr_matrix(exp.to_numpy()))
